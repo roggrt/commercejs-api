a52477b4cbca7fafdc027000622cdb5b11ba3eca
roggrt <42749056+roggrt@users.noreply.github.com>
2021-10-22 03:33:44 -0500

    Update SocialMedia.js

--- components/common/SocialMedia.js
+++ components/common/SocialMedia.js
@@ -79,12 +79,12 @@ export default class SocialMedia extends React.Component {
                 className="font-size-display1 mb-3"
                 style={{ maxWidth: '26rem' }}
               >
-                Follow us on Instagram for more updates
+                Proximamente una tienda online de belleza
               </p>
               <div className="d-flex">
                 <Link href="/about">
                   <a className="d-flex py-3 align-items-center font-color-black borderbottom border-color-black">
-                    <p className="mr-3">Follow us</p>
+                    <p className="mr-3">Siguenos para mas actualizaciones</p>
                     <img src="/icon/arrow-long-right.svg" alt="Arrow icon"/>
                   </a>
                 </Link>
