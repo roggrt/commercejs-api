4b7b63b28cf4569aaf3b9e18328d639eafe34bfc
roggrt <42749056+roggrt@users.noreply.github.com>
2021-10-22 04:23:29 -0500

    Update SuggestedProducts.js

--- components/productAssets/SuggestedProducts.js
+++ components/productAssets/SuggestedProducts.js
@@ -11,13 +11,13 @@ class SuggestedProducts extends Component {
       <div className="custom-container py-5 my-5">
         <div className="d-flex flex-column align-items-center mb-5 pb-4">
           <p className="font-color-medium mb-4">
-            Suggested products
+            Productos sugeridos
           </p>
           <p
             className="text-center font-size-display1 mb-3 font-weight-medium"
             style={{ maxWidth: '32rem' }}
           >
-            You may also like to check out these products.
+           Productos que te podrian gustar.
           </p>
         </div>
         <ProductRow products={products.slice(0, 4)} />
